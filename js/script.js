34159e091def0b7dc3696587748013d5fa99ccf3
Thelema <campermailru@mail.ru>
2021-08-02 19:04:21 +0300

    WatmelCount

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 "use strict";//cстрогий режим , режим современного программирования
+const quaWatmel = +prompt('Сколько арбузов вы уже купили?','');
+
+const perQuaWatmel = {
+count:quaWatmel,
+watmel:{},//арбуз
+kind:{},//вид
+cap:[],//качество
+cont: false//удовлетворенность
+};
+const ve = prompt('Покупали ли вы арбуз в этом году?',''),
+        vo = prompt('Какой вид арбуза(ов) вы покупали?',''),
+        vi = prompt('Какой он был по качеству?',''),
+        va = prompt('Были ли вы доволни покупкой арбуза?','');
+perQuaWatmel.watmel[ve]=vo;      
+perQuaWatmel.watmel[vi]=va;
+
+console.log(perQuaWatmel);
+
+        
+
+
+
